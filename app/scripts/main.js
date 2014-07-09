43286a61ea7124eb8d0ecabefce489b97df17bda
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-08 20:53:29 -0400

    main.js template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,36 @@
-// itemList = [];
+var todotemplate = _.template$("todo-template").html();
+$("list").append(renderedtemplate);
 
-// var Items = function(options) {
-// 	var options = options || {};
-// 	this.id = _.uniqueId(listItem);
-// 	this.active = true;
-// 	this.name = $('input[name="addItem"]').val("");
-// }
+var renderedtemplate = todotemplate(todo);
+
+var item = [];
+
+
+
+var ToDo = function(options) {
+	var options = options || {};
+	this.id = _.uniqueId(listItem);
+	this.active = true;
+	this.userinput = options.userinput;
+	}
+}
+
+
+$("buttonname").click(function () {
+	var todo = new ToDo ({userinput: $(' ').val();
+		_.push(item);
+
+	}
+})
 
 // $(function(){
 //     $('input[name="add"]').click(function() {
 // 			var value =  $('input[name="addItem"]').val();
 //     	$('ul li:last').after("<li>" + value + "</li>");
 //     	$('input[name="addItem"]').val("");
-// 			console.log(value);
+			
 //     });
 // });
 
-// console.log(itemList);
+$("list").append(renderedtemplate);
+
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -7,13 +7,23 @@
 // 	this.name = $('input[name="addItem"]').val("");
 // }
 
-// $(function(){
-//     $('input[name="add"]').click(function() {
-// 			var value =  $('input[name="addItem"]').val();
-//     	$('ul li:last').after("<li>" + value + "</li>");
-//     	$('input[name="addItem"]').val("");
-// 			console.log(value);
-//     });
-// });
+$(function(){
+    $('input[name="add"]').click(function() {
+			var value =  $('input[name="addItem"]').val();
+    	$('ul li:last').after("<li>" + value + "</li>");
+    	$('input[name="addItem"]').val("");
+			
+    });
+});
 
 // console.log(itemList);
+
+
+// var todotemplate = _.template$("todo-template").html();
+// $("list").append(renderedtemplate);
+
+// var renderedtemplate = todotemplate(todo);
+
+// var data-array = [];
+
+// $("list").append(renderedtemplate);
