bb60aa2300d852d65a4540b979dd454fa28fae8f
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-09 09:00:05 -0400

    gulp

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -22,10 +22,16 @@ $(".octicon-diff-renamed").click(function () {
 		$(".aggregate").append(todotemplate(todo));
 
 		item.push(todo);
-		console.log(item);
+		// console.log(item);
+
+ 
+
+
+		var hey = $(".octicon-x");
+		console.log(hey+"this is hey!!");
 });
 
-
+ 
 // $(function(){
 //     $('input[name="add"]').click(function() {
 // 			var value =  $('input[name="addItem"]').val();
@@ -36,9 +42,19 @@ $(".octicon-diff-renamed").click(function () {
 // });
 
 // $(".aggregate").append(todotemplate);
+$(document).ready(function () {
+		$(document).on('click', '#kill', function() {
+			console.log("I'm clicked");
+			var current_id = $(this).parents('.todo').attr('id');
+			console.log(current_id);
+			$('li#'+current_id).remove();
+			
+  		item.splice(current_id-1, 1);
+  		// console.log(item);
+  		// console.log("I'm the result");
 
-$("#kill").click(function() {
-	var current_id = $(".todo").attr(id);
-	console.log(current_id);
+		});
 });
+
+
 
